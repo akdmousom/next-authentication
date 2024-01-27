160a7cd846dbebafcdfcd8d7c6ba214e7e0fa243
AKD <contact.mousom@gmail.com>
2024-01-28 02:17:37 +0600

    [Provider](Added) Credentials Provider Is Added In Our Authentication System

--- src/app/api/auth.js
+++ src/app/api/auth.js
@@ -1,8 +1,30 @@
 import NextAuth from "next-auth"
 import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-export const {auth, handlers:{GET, POST}} = NextAuth({
+export  const  {auth, handlers:{GET, POST}} = NextAuth({
     providers: [
+
+        CredentialsProvider({
+            name: "Credentials",
+
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "User Name" },
+                password: { label: "Password", type: "password", placeholder: "Password" }
+              },
+
+              authorize: async(credentials)=> {
+
+                const user = { id: "1", name: "J Smith", email: "jsmith@example.com", username: 'akd', password:'admin' }
+
+                if (credentials.username === user.username && credentials.password === user.password ) {
+                    return user
+                }else{
+                    return null
+                }
+
+              }
+        }),
 
         GitHubProvider({
             clientId: process.env.GITHUB_ID,
