dd5570252bf5c5d34a2da10d8a4d8596e8610d05
AKD <contact.mousom@gmail.com>
2024-01-28 00:07:40 +0600

    [Added](Description) Description is added in home page.

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
       <>
       
         <div className="bg-emerald-500 min-h-screen max-w-screen grid justify-center items-center ">
-            <h1 className="text-center text-white font-extrabold text-5xl">Welcome To Next Auth</h1>
+            <h1 className="text-center text-white font-extrabold text-5xl">Welcome To Next Auth...</h1>
         </div>
       
       </>
