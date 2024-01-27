64ca5c5a43e30ffbbf59a6ca68ed60d006ee49fa
AKD <contact.mousom@gmail.com>
2024-01-28 00:30:25 +0600

    .

--- src/app/page.js
+++ src/app/page.js
@@ -2,19 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
     <>
 
       <div className="bg-emerald-500 min-h-screen max-w-screen grid justify-center items-center ">
         <div>
           <h1 className="text-center text-white font-extrabold text-5xl">Welcome To Next Auth...</h1>
           <p className="py-4 text-2xl font-bold">With this project you can learn how to apply next authentication system in your next 14 project</p>
-=======
-      <>
-      
-        <div className="bg-emerald-500 min-h-screen max-w-screen grid justify-center items-center ">
-            <h1 className="text-center text-white font-extrabold text-5xl">Welcome To Next Auth.</h1>
->>>>>>> stage
         </div>
       </div>
 
