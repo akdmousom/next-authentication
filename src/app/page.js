babbd259ecdcc8503b314412a893f74ba09a22c1
AKD <contact.mousom@gmail.com>
2024-01-28 00:14:52 +0600

    [Added](Description) Description is added in home page..

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <>
-      
-        <div className="bg-emerald-500 min-h-screen max-w-screen grid justify-center items-center ">
-            <h1 className="text-center text-white font-extrabold text-5xl">Welcome To Next Auth...</h1>
+    <>
+
+      <div className="bg-emerald-500 min-h-screen max-w-screen grid justify-center items-center ">
+        <div>
+          <h1 className="text-center text-white font-extrabold text-5xl">Welcome To Next Auth...</h1>
+          <p className="py-4 text-2xl font-bold">With this project you can learn how to apply next authentication system in your next 14 project</p>
         </div>
-      
-      </>
+      </div>
+
+    </>
   );
 }
