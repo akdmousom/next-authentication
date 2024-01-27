b826badf8a3056a9ade692864652be985da15a6c
AKD <contact.mousom@gmail.com>
2024-01-28 01:50:57 +0600

    [Authentication](Done) I add a logic in my app route page this logic help me to do privet route. But you have to create a session first otherwise you can't make your route privet

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,23 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
+import { auth } from "./api/auth";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await auth();
+
+  if (!session) redirect('/api/auth/signin')
+
   return (
     <>
-      <div className="min-h-screen max-w-screen grid justify-center items-center bg-green-500">
+      <div className="min-h-screen max-w-screen grid justify-center items-center bg-gray-600">
 
-        <div>
-          <h1 className="text-3xl font-extrabold text-center">Welcome To The <span className="text-red-600">Next Auth</span> System</h1>
-          <p className="py-4 text-2xl text-black font-semibold">With this project you can learn how to implement the next authentication system in next js 14</p>
+        <div className="text-center">
+          <h1 className="text-3xl text-white font-extrabold text-center">Welcome To The <span className="text-red-600">Next Auth</span> System</h1>
+          <p className="py-4 text-2xl text-white font-semibold">With this project you can learn how to implement the next authentication system in next js 14</p>
+          <Link href={'/api/auth/signout'}><button className="bg-gray-900 text-white px-4 py-4 rounded-lg hover:bg-red-400 transform duration-300">Sign Out</button></Link>
         </div>
+
 
 
 
